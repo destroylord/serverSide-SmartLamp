9b58f71c71e908168ed5e7e5abe7a7d67cc61fa9
destroylord <masapin68@gmail.com>
2023-01-10 00:41:31 +0700

    chore: setting codition for module Relay

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const http = require('http')
 const { Server } = require('socket.io')
 const cors = require('cors')
 
-const { Board, Led } = require('johnny-five')
-const board = new Board()
+const five = require('johnny-five')
+const board = new five.Board({ repl: true })
 
 app.use(cors())
 
@@ -29,23 +29,41 @@ app.get('/', function (req, res) {
 io.on('connection', (socket) => {
   console.log(`User connected: ${socket.id}`)
 
-  // Source LED
   board.on('ready', () => {
     socket.on('ping', () => {
-      var led = new Led(13)
-      led.on()
+      var relay = new five.Relay({ pin: 13 }) // default type:
+
+      relay.open()
       console.log('detection Manusia')
     })
   })
 
   board.on('ready', () => {
     socket.on('dead', () => {
-      var led = new Led(13)
-      led.off()
-      console.log('Lampunya mati')
+      var relay = new five.Relay({ pin: 13 }) // default type:
+
+      relay.close()
+      console.log('Manusianya Gak Ada')
     })
   })
 })
+// Source LED
+//   board.on('ready', () => {
+//     socket.on('ping', () => {
+//       var led = new Led(13)
+//       led.on()
+//       console.log('detection Manusia')
+//     })
+//   })
+
+//   board.on('ready', () => {
+//     socket.on('dead', () => {
+//       var led = new Led(13)
+//       led.off()
+//       console.log('Lampunya mati')
+//     })
+//   })
+// })
 
 server.listen(5000, () => {
   console.log('listening on http://127.0.0.1:5000/')
